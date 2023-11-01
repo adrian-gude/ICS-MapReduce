339d7da86addbbe23f2fbf9b1e70e8c284665bdf
adrian-gude <adrianlopezgude@gmail.com>
2023-11-01 11:17:10 +0100

    P1

--- mapper.py
+++ mapper.py
@@ -1,31 +1,22 @@
-'''#!/usr/bin/env python
-import sys
-# input comes from STDIN (standard input)
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-    # split the line into words
-    words = line.split()
-    # increase counters
-    for word in words:
-        # write the results to STDOUT (standard output).
-        # tab-delimited; the trivial word count is 1
-        print '%s\t%s' % (word, 1)
-'''
 #!/usr/bin/env python
 import sys
+import os
+
+file_name=os.environ['mapreduce_map_input_file']
 
 for line in sys.stdin:
-    # Dividir la línea en campos
-    fields = line.strip().split()
     
-    if len(fields) == 24:
-        station_id = fields[0]
-        date = fields[1]
-        temperature = float(fields[2])
+
+    line = line.strip()
+    words = line.split()
+
+    if len(words) >= 4 or words[11] != 'U':
+
+        max_temperature = float(words[5])
+        min_temperature = float(words[6])
         
-        if date.startswith("2017") and temperature != -9999.0:
-            if temperature > 27.0:
-                print '%s\t%s' % (station_id,temperature)
-            elif temperature < -10.0:
-                print '%s\t%s' % (station_id,temperature)
+        if max_temperature > 27.0:
+            print '%s\t%s\t%s' % ("hot",max_temperature,file_name)
+
+        elif min_temperature < -10.0:
+            print '%s\t%s\t%s' % ("cold",min_temperature,file_name)
--- reducer.py
+++ reducer.py
@@ -1,56 +1,23 @@
-'''#!/usr/bin/env python
-import sys
-current_word = None
-current_count = 0
-word = None
-# input comes from STDIN
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-    # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-    # count was not a number, so silently. Ignore/discard this line continue
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
-        current_word = word
-# do not forget to output the last word if needed!
-if current_word == word:
-print '%s\t%s' % (current_word, current_count)
-'''
 #!/usr/bin/env python
 import sys
 
+max_hot_temp = 0
+max_cold_temp = 0
 
-max_hot_city = None
-max_hot_temp = -float('inf')
-max_cold_city = None
-max_cold_temp = float('inf')
+
 
 for line in sys.stdin:
-    fields = line.strip().split("\t")
-    station_id = fields[0]
-    temperature = float(fields[1])
-    condition = fields[2]
+    words = line.strip().split("\t")
+    
+    temperature = float(words[1])
+    condition = words[0]
+    file_name = words[2]
 
-    if condition == "Hot" and temperature > max_hot_temp:
-        max_hot_city = station_id
+    if condition == "hot" and temperature > max_hot_temp:
         max_hot_temp = temperature
-    elif condition == "Cold" and temperature < max_cold_temp:
-        max_cold_city = station_id
+
+    elif condition == "cold" and temperature < max_cold_temp:
         max_cold_temp = temperature
 
-if max_hot_city:
-    print '%s\t%s' % ('hotest',max_hot_city)
-if max_cold_city:
-    print '%s\t%s' % ('coldest',max_cold_city)
+print '%s\t%s\t%s' % ('hotest',max_hot_temp,file_name)
+print '%s\t%s\t%s' % ('coldest',max_cold_temp,file_name)
