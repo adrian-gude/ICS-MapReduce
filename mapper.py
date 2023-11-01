208bb37700ad6487ebcb92a3e047378fe1bcbf6c
adrian-gude <adrianlopezgude@gmail.com>
2023-11-01 12:22:13 +0100

    adding

--- mapper.py
+++ mapper.py
@@ -1,22 +1,8 @@
-'''#!/usr/bin/env python
-import sys
-# input comes from STDIN (standard input)
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-    # split the line into words
-    words = line.split()
-    # increase counters
-    for word in words:
-        # write the results to STDOUT (standard output).
-        # tab-delimited; the trivial word count is 1
-        print '%s\t%s' % (word, 1)
-'''
 #!/usr/bin/env python
 import sys
 
 for line in sys.stdin:
-    # Dividir la línea en campos
+    # Dividir la linea en campos
     fields = line.strip().split()
     
     if len(fields) == 24:
